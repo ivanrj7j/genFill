176d484e6eeb3eb67a707b2d730c529f835a510c
ivanrj7j <ivanrj7j@gmail.com>
2024-11-09 09:19:55 +0530

    changed the loss functions to be autocast safe

--- src/losses.py
+++ src/losses.py
@@ -4,21 +4,19 @@ This file contains some loss functions for models
 
 import torch
 
-def discriminatorLoss(inputReal:torch.Tensor, inputFake:torch.Tensor, ouptputReal:torch.Tensor, ouptputFake:torch.Tensor, entropyNoise:float = 5e-2):
+def discriminatorLoss(ouptputReal:torch.Tensor, ouptputFake:torch.Tensor, entropyNoise:float = 5e-2):
     """
     Loss function for the discriminator.
     
     Parameters:
-    inputReal (torch.Tensor): Real input tensor with shape (batchSize, channels, height, width).
-    inputFake (torch.Tensor): Fake input tensor with shape (batchSize, channels, height, width).
     ouptputReal (torch.Tensor): Output tensor from the discriminator for real inputs with shape (batchSize, 1, m, n).
     ouptputFake (torch.Tensor): Output tensor from the discriminator for fake inputs with shape (batchSize, 1, m, n).
     entropyNoise (float): Entropy noise to add to the loss. Defaults to 5e-2.
     """
 
     noise = torch.rand_like(ouptputReal) * entropyNoise
-    realLoss = torch.mean(torch.nn.functional.binary_cross_entropy(ouptputReal, torch.ones_like(ouptputReal)-noise))
-    fakeLoss = torch.mean(torch.nn.functional.binary_cross_entropy(ouptputFake, torch.zeros_like(ouptputFake)+noise))
+    realLoss = torch.mean(torch.nn.functional.binary_cross_entropy_with_logits(ouptputReal, torch.ones_like(ouptputReal)-noise))
+    fakeLoss = torch.mean(torch.nn.functional.binary_cross_entropy_with_logits(ouptputFake, torch.zeros_like(ouptputFake)+noise))
 
     return realLoss+fakeLoss
 
@@ -29,4 +27,4 @@ def generatorLoss(discriminatorOutput:torch.Tensor):
     Parameter:
     discriminatorOutput (torch.Tensor): Output tensor from the discriminator for the generated image with shape (batchSize, 1, m, n).
     """
-    return torch.mean(torch.nn.functional.binary_cross_entropy(discriminatorOutput, torch.ones_like(discriminatorOutput)))
+    return torch.mean(torch.nn.functional.binary_cross_entropy_with_logits(discriminatorOutput, torch.ones_like(discriminatorOutput)))
